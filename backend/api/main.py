02f549551be017e316e41d59ce53a8c45651df06
Hrishikesh <hrishikeshshendage313@gmail.com>
2026-05-23 20:40:08 +0530

    UI is here

--- backend/api/main.py
+++ backend/api/main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
 from .routers import ingest, retrieval
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],          
+    allow_methods=["GET", "POST", "OPTIONS"],
+    allow_headers=["*"],
+)
 
 app.include_router(ingest.router)
 app.include_router(retrieval.router)
--- backend/api/routers/retrieval.py
+++ backend/api/routers/retrieval.py
@@ -1,16 +1,21 @@
 from fastapi import APIRouter
 from services import retrieval
+from pydantic import BaseModel
+
+class QueryRequest(BaseModel):
+    query: str
+    repo: str
 
 router = APIRouter(
     prefix="/retrieval",
     tags=["retrieval"]
 )
 
-@router.get("/{query}")
-def query_retrieval(query: str, repo: str):
+@router.post("/")
+def query_retrieval(request: QueryRequest):
     """Endpoint to trigger the retrieval pipeline for a given query and repository."""
     try:
-        response = retrieval.retrieval_pipeline(query, repo)
+        response = retrieval.retrieval_pipeline(request.query, request.repo)
     except Exception as e:
         return {"error": str(e)}
     return {"response": response.content}
